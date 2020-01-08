80672e1c558a937470fc2c934a8e4f13103deea4
ezenTour <chltkdgus092@naver.com>
2020-01-08 12:43:09 +0900

    MANAGER초반 작업
    2020-01-08 12:42 by 최상현

---
+++ ezen_tour/src/main/java/com/ezen/tour/manager/model/ManagerVO.java
@@ -0,0 +1,54 @@
+package com.ezen.tour.manager.model;
+
+public class ManagerVO {
+	private String adminId;
+	private String adminPwd;
+	private String name;
+	private String email;
+	private String hp;
+	private String position;
+	
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminPwd() {
+		return adminPwd;
+	}
+	public void setAdminPwd(String adminPwd) {
+		this.adminPwd = adminPwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	@Override
+	public String toString() {
+		return "ManagerVO [adminId=" + adminId + ", adminPwd=" + adminPwd + ", name=" + name + ", email=" + email
+				+ ", hp=" + hp + ", position=" + position + "]";
+	}
+	
+}
