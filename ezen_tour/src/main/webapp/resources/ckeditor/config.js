45316153a2bc3f0e435f76fab2782a07eef95055
ezenTour <chltkdgus092@naver.com>
2020-01-23 13:13:57 +0900

    ckeditor 대량 수정
    2020-01-23 13:15 by 최상현

--- ezen_tour/src/main/webapp/resources/ckeditor/config.js
+++ ezen_tour/src/main/webapp/resources/ckeditor/config.js
@@ -4,18 +4,25 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	CKEDITOR.config.allowedContent = true;
-	CKEDITOR.filebrowserUploadMethod = 'form';
-	CKEDITOR.on('dialogDefinition', function( ev ){
-	    var dialogName = ev.data.name;
-	    var dialogDefinition = ev.data.definition;
-	 
-	    switch (dialogName) {
-	    	case 'image': //Image Properties dialog
-	        	//dialogDefinition.removeContents('info');
-	            dialogDefinition.removeContents('Link');
-	            dialogDefinition.removeContents('advanced');
-	            break;
-	    }
-	});
+	config.language = 'ko';
+	
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Source,Templates,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CopyFormatting,RemoveFormat,NumberedList,BulletedList,Indent,Outdent,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Flash';
 };
