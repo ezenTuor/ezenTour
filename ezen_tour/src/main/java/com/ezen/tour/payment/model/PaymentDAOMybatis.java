10522e9b5549fa40a56b0cf814df17d8c4bab0dc
ezenTour <chltkdgus092@naver.com>
2020-02-04 17:07:01 +0900

    오류 수정중
    2020-02-04 19:06

---
+++ ezen_tour/src/main/java/com/ezen/tour/payment/model/PaymentDAOMybatis.java
@@ -0,0 +1,19 @@
+package com.ezen.tour.payment.model;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PaymentDAOMybatis implements PaymentDAO{
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	private String namespace = "config.mybatis.mapper.oracle.payment.";
+
+	@Override
+	public int insertPayment(PaymentVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
